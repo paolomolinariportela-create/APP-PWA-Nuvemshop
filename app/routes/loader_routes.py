f15506769424eec0d3845c696e675204bd2b50fa
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-02-17 21:27:47 -0300

    Update loader_routes.py

--- app/routes/loader_routes.py
+++ app/routes/loader_routes.py
@@ -208,8 +208,8 @@ def get_loader(store_id: str, request: Request, db: Session = Depends(get_db)):
 
                     var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
                     svg.setAttribute('viewBox', '0 0 24 24');
-                    svg.setAttribute('width', '20');
-                    svg.setAttribute('height', '20');
+                    svg.setAttribute('width', '24');
+                    svg.setAttribute('height', '24');
                     svg.style.fill = 'currentColor';
 
                     var path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
