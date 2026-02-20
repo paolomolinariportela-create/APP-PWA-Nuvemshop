992d66179afd6033a7ab2f3b265f30dab1d89f07
paolomolinariportela-create <paolomolinariportela@gmail.com>
2026-02-20 17:45:32 -0300

    Update loader_routes.py

--- app/routes/loader_routes.py
+++ app/routes/loader_routes.py
@@ -372,7 +372,7 @@ def get_loader(store_id: str, request: Request, db: Session = Depends(get_db)):
             }}
             try {{
                 console.log("PUSH: registrando Service Worker em:", '{final_backend_url}/service-worker.js');
-                const registration = await navigator.serviceWorker.register('{final_backend_url}/service-worker.js', {{ scope: '/' }});
+               const registration = await navigator.serviceWorker.register('/service-worker.js', { scope: '/' });
                 await navigator.serviceWorker.ready;
 
                 console.log("PUSH: chamando pushManager.subscribe...");
